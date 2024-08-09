7b3e5ae95a8645c59e2a25764e14954503447583
Abdullah Kara <91596198+abdullahkra@users.noreply.github.com>
2024-08-10 01:58:53 +0300

    Update main.dart

--- breathe_app/lib/main.dart
+++ breathe_app/lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
     _fadeInText();
   }
 
-//apollokapollo
+//apollokapollorusto
   void _fadeInText() {
     Future.delayed(Duration(seconds: 2), () {
       setState(() {
