899c84b65bf08434d97672f25147d6932fc2b573
Abdullah Kara <91596198+abdullahkra@users.noreply.github.com>
2024-08-10 00:41:56 +0300

    animatedText

--- breathe_app/lib/main.dart
+++ breathe_app/lib/main.dart
@@ -1,21 +1,84 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fadeInText();
+  }
+
+  void _fadeInText() {
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return MaterialApp(
       home: Scaffold(
-        body: SizedBox.expand(
-          child: Image.asset(
-            "assets/images/1.jpg",
-            fit: BoxFit.cover,
-          ),
+        body: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.asset(
+                'assets/images/1.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.15,
+              left: 0,
+              right: 0,
+              child: AnimatedOpacity(
+                opacity: _opacity,
+                duration: Duration(seconds: 3),
+                child: Text(
+                  'Hello',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenWidth * 0.07,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: screenHeight * 0.23,
+              left: 0,
+              right: 0,
+              child: AnimatedOpacity(
+                opacity: _opacity,
+                duration: Duration(seconds: 8),
+                child: Text(
+                  "Let's start relaxing",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.07,
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
