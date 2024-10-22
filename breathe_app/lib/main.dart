40e61ee7c93e92a65139b8842d7db0c12158a173
Muhammed <130292856+Russtamov@users.noreply.github.com>
2024-10-22 14:16:31 +0300

    update main

--- breathe_app/lib/main.dart
+++ breathe_app/lib/main.dart
@@ -1,22 +1,9 @@
 import 'package:breathe_app/core/init/meditation_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'feature/view/landing_page/landing_page.dart';
 
-// Firebase arka plan mesajlarını işleyen fonksiyon
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print("Handling a background message: ${message.messageId}");
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
+void main() {
   runApp(MeditationApp());
 }
 
@@ -27,7 +14,7 @@ class MeditationApp extends StatelessWidget {
       create: (_) => MeditationProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: LandingPage(),
+        home: LandingPage(), // LandingPage ana sayfa olarak ayarlandı
       ),
     );
   }
